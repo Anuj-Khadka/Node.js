39c45b30b4a9d4643cb461f570f8f3646d9eebdc
Anuj-Khadka <anujkhadka008@gmail.com>
2024-01-16 20:23:06 -0600

    Add Lecture 3

---
+++ 3. Request and Response/server.js
@@ -0,0 +1,74 @@
+const http = require('http')
+const fs = require('fs')
+
+let server = http.createServer((req, res) => {
+    // console.log('request made\n', req)
+    console.log(req.url, req.method)
+
+    // text type
+    // set header content type 
+    // res.setHeader('Content-Type', 'text/plain')
+
+    // send res to the browser
+    // res.write("hello world.")
+
+
+    // type html
+    res.setHeader('Content-Type', 'text/html')
+
+    // res.write('<h1>Hello world</h1>')
+
+    // res.end()
+
+
+    
+
+    let path = './views/';
+
+    switch (req.url) {
+        case '/':
+            path += 'index.html'
+            res.statusCode = 200
+            break;
+
+        case '/about':
+            path += 'about.html'
+            res.statusCode = 200
+            break;
+        
+        case '/about-me':
+            res.statusCode = 301
+            res.setHeader('Location', '/about')
+            res.end()
+            break;
+
+        case '/contact':
+            path += 'contact.html'
+            res.statusCode = 200
+            break;
+
+        default:
+            path += '404.html';
+            res.statusCode = 404
+            break;
+    }
+
+    // render html page 
+    // fs.readFile('./views/index.html', (err, docs) => {
+        fs.readFile(path, (err, docs) => {                          // multiple routes
+            if (err) {
+                console.log('error', err)
+                res.end()
+            }
+            else {
+                // res.write(docs)
+                res.end(docs)           // shorthand
+                console.log(res.statusCode)
+            }
+        })
+
+})
+
+server.listen(3000, 'localhost', () => {
+    console.log('server has been listening')
+})
